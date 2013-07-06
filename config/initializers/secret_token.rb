0384119b8e77d5cf3ff4a5c21bc4823d997e52f2
william xiao <willy.xiao1989@gmail.com>
2013-07-06 16:54:11 +0800

    first git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+William1::Application.config.secret_token = '0f87b9b20106b00f9319fe81de364658637c130f47ee81e89a67af06a4359f698961e4afcc281a44dbda2fc2ac6b8fbc8b79ab4197a206ca75494b34cd388430'
